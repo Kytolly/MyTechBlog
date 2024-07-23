22831ac5aa45b0eabb86dc334f3d6d254639dfb7
Kytolly <1392875097@qq.com>
2024-07-23 19:54:57 +0800

    完成对添加用户，查询用户列表的设计

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,21 +1,64 @@
 package v1
 
 import (
+	//"fmt"
+	"log/slog"
+	"mytechblog/model"
+	msg "mytechblog/utils/errormsg"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
-	msg "mytechblog/utils/errormsg" 
 )
+
 // 查询用户是否已存在
 func UserExist(c *gin.Context)int{
-	return msg.SUCCESS
+	return 1
 }
 // 添加用户
+// 不允许相同名字的用户写入
 func AddUser(c *gin.Context){
+	slog.Debug(c.FullPath())
+
+	var data model.User
+	err := c.ShouldBindJSON(&data)
+	if err != nil{
+		slog.Error("Bind JSON Error")
+	}
+	slog.Debug(data.Username)
+
+	status := model.CheckUser(data.Username)
+
+	if status == msg.SUCCESS{
+		model.CreateUser(&data)
+	}
+	if status == msg.ERROR_USERNAME_USED{
+		slog.Error("User already exists")
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": status,
+		"data": data,
+		"message": msg.GetErrorMessage(status),
+	})
 }
 // 查询用户
 // 查询单个用户
 
 // 查询用户列表
 func GetUsers(c *gin.Context){
+	pageSize,_ := strconv.Atoi(c.Query("pagesize"))
+	pageNumber,_ := strconv.Atoi(c.Query("pagenumber"))
+	if pageNumber == 0{
+		pageNumber = 1
+	}
+
+	data := model.GetUsers(pageSize, pageNumber)
+	status := msg.SUCCESS
+	c.JSON(http.StatusOK, gin.H{
+		"status": status,
+		"data": data,
+		"message": msg.GetErrorMessage(status),
+	})
 }
 // 更新用户
 func UpdateUser(c *gin.Context){
--- model/user.go
+++ model/user.go
@@ -1,7 +1,8 @@
 package model
 
 import (
-	//"mytechblog/utils"
+	"log/slog"
+	msg "mytechblog/utils/errormsg"
 	//"fmt"
 	// "gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -14,3 +15,37 @@ type User struct{
 	Password string `gorm:"type:varchar(500);not null" json:"password"`
 	Role 	 int 	`gorm:"type:int" json:"role"`
 }
+
+// 查询用户是否已存在
+func CheckUser(name string)int{
+	var users User
+	db.Select("id").Where("username=?", name).First(&users)
+	if users.ID > 0{
+		slog.Warn("User already exists") 
+		return msg.ERROR_USERNAME_USED
+	}
+	return msg.SUCCESS
+}
+
+// 添加用户
+func CreateUser(data *User)int{
+	err := db.Create(&data).Error
+	if err != nil{
+		slog.Error("Failed to create user")
+		return msg.ERROR
+	}
+	return msg.SUCCESS
+}
+
+// 查询用户列表
+func GetUsers(pageSize int, pageNumber int)[]User{
+	var users []User
+	err := db.Limit(pageSize).Offset((pageNumber-1)*pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound{
+		slog.Info("Failed to get users", "error", err)
+		return nil
+	}
+	return users
+}
+
+// 编辑用户

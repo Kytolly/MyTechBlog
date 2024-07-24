3db2bbdc863cddf0d757f6169fccc6c73d3d78dc
Kytolly <1392875097@qq.com>
2024-07-24 13:43:10 +0800

    完成对用户密码加密后存储进数据库

--- model/user.go
+++ model/user.go
@@ -1,12 +1,15 @@
 package model
 
 import (
+	"encoding/base64"
 	"log/slog"
 	msg "mytechblog/utils/errormsg"
+
 	//"fmt"
 	// "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	//"log/slog"
+	"golang.org/x/crypto/scrypt"
 )
 
 type User struct{
@@ -29,6 +32,7 @@ func CheckUser(name string)int{
 
 // 添加用户
 func CreateUser(data *User)int{
+	data.Password = ScryptPassword(data.Password)
 	err := db.Create(&data).Error
 	if err != nil{
 		slog.Error("Failed to create user")
@@ -49,3 +53,18 @@ func GetUsers(pageSize int, pageNumber int)[]User{
 }
 
 // 编辑用户
+
+//
+
+// 密码加密
+func ScryptPassword(password string)string{
+	const Keylen = 10
+	salt := []byte{2, 3, 5, 7, 11, 13, 17, 19}
+
+	HashPassword, err :=scrypt.Key([]byte(password), salt, 1<<15, 8, 1, Keylen)
+	if err != nil {
+		slog.Error("Failed to encrypt password", "error", err)
+	}
+	FinalPassword := base64.StdEncoding.EncodeToString(HashPassword)
+	return FinalPassword
+}
